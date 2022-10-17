4adc2b371a193f3d10ee41b5ad6fbe5549c4e042
Gefim1 <geo969@yandex.ru>
2022-10-18 02:10:44 +0300

    2345
    123

---
+++ Знакомство с языками программирования (семинары)/второй семинар/Program.cs
@@ -0,0 +1,19 @@
+﻿//на вход 2 числа и проверяет является ли оно квадратом другого 3,9 5,25 -4
+
+Console.Write("Введите первое число ");
+
+int number1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число ");
+
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 * number1 == number2 || number2 * number2 == number1)
+{
+    Console.Write("да ");
+
+}
+else
+{
+    Console.Write("нет ");
+}
