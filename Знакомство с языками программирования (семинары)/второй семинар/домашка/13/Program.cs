354a2b7bfc934667fa8472944e8af4ffd9e3b81c
Gefim1 <geo969@yandex.ru>
2022-10-21 00:40:52 +0300

    ь

---
+++ Знакомство с языками программирования (семинары)/второй семинар/домашка/13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+while (num > 1000)
+{
+    num /= 10;
+}
+int num1 = num % 10;
+
+if (num < 3)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    Console.WriteLine($"Третья цифра это: {num1}");
+}
---
+++ Знакомство с языками программирования (семинары)/второй семинар/домашка/15/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.WriteLine("Введите номер дня недели: ");
+int day = int.Parse(Console.ReadLine()!);
+
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("Выходной день. ");
+}
+else if (day > 7)
+{
+    Console.WriteLine("Повторите запрос. ");
+}
+else 
+{
+    Console.WriteLine("Будний день. ");
+}
