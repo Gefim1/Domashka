eaba8a922401e9729846c8b6025f162299842d82
Gefim1 <geo969@yandex.ru>
2022-12-11 17:08:20 +0300

    итоговая работа

--- Знакомство с языками программирования (семинары)/9й семинар/домашка/задача 64/Program.cs
+++ Знакомство с языками программирования (семинары)/9й семинар/домашка/задача 64/Program.cs
@@ -10,15 +10,15 @@ Console.WriteLine(NaturalNumber(n, m));
 
 int NaturalNumber(int n, int m)
 {
- if (n == m)
- return n;
- else
- Console.Write($"{NaturalNumber(n, m + 1)}, ");
- return m;
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
 }
 
 int InputInt(string output)
 {
- Console.Write(output);
- return int.Parse(Console.ReadLine());
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
 }
--- Знакомство с языками программирования (семинары)/9й семинар/домашка/задача 68/Program.cs
+++ Знакомство с языками программирования (семинары)/9й семинар/домашка/задача 68/Program.cs
@@ -10,16 +10,16 @@ Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
 
 int InputInt(string output)
 {
- Console.Write(output);
- return int.Parse(Console.ReadLine());
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
 }
 
 int Akkerman(int m, int n)
 {
- if (m == 0)
- return n + 1;
- if (m > 0 && n == 0)
- return Akkerman(m - 1, 1);
- else
- return Akkerman(m - 1, Akkerman(m, n - 1));
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
 }
